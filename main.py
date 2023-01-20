018c8d560a89eb10bead6dfd3d1746ee14229619
Thomas Yuan <thomas@LAPTOP-LBE96NCJ.localdomain>
2023-01-20 10:59:24 +0000

    Changes to main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from src.text_classifier import text_classifier_main
+from src.models import text_classify, seq2seq_translate, Synonym_Replacer, Back_Translator
+import random, torch, numpy
+import pytorch_lightning.utilities.seed as plseed
+from googletrans import Translator
+
+SEED = 42
+random.seed(SEED)
+numpy.random.seed(SEED)
+g = torch.Generator()
+g.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)               
+torch.cuda.manual_seed_all(SEED)
+plseed.seed_everything(SEED)
 
 if __name__ == "__main__":
-    text_classifier_main()
+
+    # accuracy = text_classify(Back_Translator("en", "de"))
+
+    seq2seq_translate()
+
+    # python main.py --devices=-1 --accelerator="auto" --max_epochs=20 --auto_scale_batch_size="binsearch" --auto_select_gpus=True

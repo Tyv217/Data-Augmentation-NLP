e180f2d7e2ab48a3f5b293ddc437449c188377fd
Thomas Yuan <thomas@LAPTOP-LBE96NCJ.localdomain>
2023-01-20 15:53:10 +0000

    1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.models import text_classify, seq2seq_translate, Synonym_Replacer, Back_
 import random, torch, numpy
 import pytorch_lightning.utilities.seed as plseed
 from googletrans import Translator
+import pdb
 
 SEED = 42
 random.seed(SEED)
@@ -13,9 +14,9 @@ torch.cuda.manual_seed_all(SEED)
 plseed.seed_everything(SEED)
 
 if __name__ == "__main__":
+    pdb.set_trace()
+    accuracy = text_classify(Back_Translator("en", "de"))
 
-    # accuracy = text_classify(Back_Translator("en", "de"))
-
-    seq2seq_translate()
+    # seq2seq_translate()
 
     # python main.py --devices=-1 --accelerator="auto" --max_epochs=20 --auto_scale_batch_size="binsearch" --auto_select_gpus=True

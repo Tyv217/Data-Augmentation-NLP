f2dfbaee3a42348e23af208f51fdaa2f1920bac0
Thomas <thomasyuan1@gmail.com>
2023-02-12 21:58:26 +0000

    twitter data

--- src/data/glue_data.py
+++ src/data/glue_data.py
@@ -27,7 +27,7 @@ class GlueDataModule(pl.LightningDataModule):
         input_lines = []
         labels = []
         for i in data:
-            input_lines.append(i['text'])
+            input_lines.append(i['sentence'])
             labels.append(i['label'])
         return input_lines, labels
 

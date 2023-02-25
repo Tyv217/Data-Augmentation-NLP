a31981f4c2830276242e9486304b11f1e792822f
Thomas <thomasyuan1@gmail.com>
2023-02-25 18:25:44 +0000

    change size

--- src/helpers/plot_embeddings.py
+++ src/helpers/plot_embeddings.py
@@ -7,8 +7,8 @@ def plot_and_compare_emb(embeddings1, embeddings2, fig_name):
     reduced2 = pca.fit_transform(embeddings2)
     reduced1 = reduced1.transpose()
     reduced2 = reduced2.transpose()
-    plt.scatter(reduced1[0], reduced1[1], c='blue')
-    plt.scatter(reduced2[0], reduced2[1], c='orange')
+    plt.scatter(reduced1[0], reduced1[1], s=5, c='blue')
+    plt.scatter(reduced2[0], reduced2[1], s=5, c='orange')
     plt.savefig(fig_name)
     plt.show()
     

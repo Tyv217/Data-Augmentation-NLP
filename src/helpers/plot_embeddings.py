3e5ad97df36746f02e10ed03dfe2f178f2cea94e
Thomas <thomasyuan1@gmail.com>
2023-02-26 19:01:11 +0000

    yes

--- src/helpers/plot_embeddings.py
+++ src/helpers/plot_embeddings.py
@@ -38,7 +38,7 @@ def plot_emb(embeddings, fig_name):
     ax.set_ylim(ymin=-abs_max, ymax=abs_max)
     ax.set_xlim(xmin=-abs_max, xmax=abs_max)
     
-    plt.text(0,0.45,text,
+    plt.text(0.45,0.45,text,
         horizontalalignment='center',
         verticalalignment='center')
     plt.savefig(fig_name)

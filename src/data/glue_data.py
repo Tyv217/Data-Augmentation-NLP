9492aba835a2f1dff9967226dde63f244bb6b32b
Thomas <thomasyuan1@gmail.com>
2023-03-03 14:38:45 +0000

    small fix

--- src/data/glue_data.py
+++ src/data/glue_data.py
@@ -28,8 +28,6 @@ class GlueDataModule(pl.LightningDataModule):
         labels = []
         for i in data:
             input_lines.append(i['sentence'])
-            label = i['label']
-            
             labels.append(i['label'])
         return input_lines, np.identity(len(self.id2label))[labels]
 

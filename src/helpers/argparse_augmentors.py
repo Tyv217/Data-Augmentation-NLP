7f999b8e0bd61a0a0718765ef27e22adf8160191
Thomas <thomasyuan1@gmail.com>
2023-04-06 22:54:10 +0100

    yes

--- src/helpers/argparse_augmentors.py
+++ src/helpers/argparse_augmentors.py
@@ -2,78 +2,84 @@ from ..models.data_augmentors import Synonym_Replacer, Back_Translator, Insertor
 AUGMENTATOR_MAPPING = {"sr": Synonym_Replacer("english"), "bt": Back_Translator("en"), "in": Insertor("english"), "de": Deletor(), "co": CutOut(), "cm": CutMix(), "mu": MixUp()}
 
 def parse_augmentors(args):
-    augmentor_names = filter(lambda x: x != "", (args.augmentors.split(",")))
-    dataset = args.dataset
+    augmentor_names = args.augmentors
+    
+    if args.task == "classify":
+        task = args.dataset
+    else:
+        task = args.task
 
     augmentation_param_mapping = {
         "ag_news": {
-            "sr": 0,
-            "in": 0,
-            "de": 24,
-            "sr,in,de": 0,
-            "bt": 0,
-            "co": 0,
-            "mu": 0,
-            "cm": 0,
+            "sr": [0],
+            "in": [0],
+            "de": [24],
+            "sr,in,de": [0,0,0],
+            "bt": [0],
+            "co": [0],
+            "mu": [0],
+            "cm": [0],
         },
         "babe": {
-            "sr": 0,
-            "in": 0,
-            "de": 0,
-            "sr,in,de": 0,
-            "bt": 0,
-            "co": 0,
-            "mu": 0,
-            "cm": 0,
+            "sr": [40],
+            "in": [20],
+            "de": [35],
+            "sr,in,de": [5,40,30],
+            "bt": [0],
+            "co": [25],
+            "mu": [25],
+            "cm": [30],
         },
         "glue": {
-            "sr": 0,
-            "in": 0,
-            "de": 0,
-            "sr,in,de": 0,
-            "bt": 0,
-            "co": 0,
-            "mu": 0,
-            "cm": 0,
+            "sr": [0],
+            "in": [0],
+            "de": [0],
+            "sr,in,de": [0,0,0],
+            "bt": [0],
+            "co": [0],
+            "mu": [0],
+            "cm": [0],
         },
         "trec": {
-            "sr": 0,
-            "in": 0,
-            "de": 0,
-            "sr,in,de": 0,
-            "bt": 0,
-            "co": 0,
-            "mu": 0,
-            "cm": 0,
+            "sr": [0],
+            "in": [0],
+            "de": [0],
+            "sr,in,de": [0,0,0],
+            "bt": [0],
+            "co": [0],
+            "mu": [0],
+            "cm": [0],
         },
         "translate": {
-            "sr": 0,
-            "in": 0,
-            "de": 0,
-            "sr,in,de": 0,
-            "bt": 0,
-            "co": 0,
-            "mu": 0,
-            "cm": 0,
+            "sr": [0],
+            "in": [0],
+            "de": [0],
+            "sr,in,de": [0,0,0],
+            "bt": [0],
+            "co": [0],
+            "mu": [0],
+            "cm": [0],
         },
         "language_model": {
-            "sr": 0,
-            "in": 0,
-            "de": 0,
-            "sr,in,de": 0,
-            "bt": 0,
-            "co": 0,
-            "mu": 0,
-            "cm": 0,
+            "sr": [0],
+            "in": [0],
+            "de": [0],
+            "sr,in,de": [0,0,0],
+            "bt": [0],
+            "co": [0],
+            "mu": [0],
+            "cm": [0],
         },
     }
 
     augmentors_on_words = []
     augmentors_on_tokens = []
     
-    for a in augmentor_names:
-        augmentor = AUGMENTATOR_MAPPING[a]
-        augmentor.set_augmentation_percentage(augmentation_param_mapping[dataset][a] / 100)
+    augmentation_params = augmentation_param_mapping[augmentor_names]
+
+    for p, n in zip(augmentation_params, list(filter(lambda x: x != "", (args.augmentors.split(","))))):
+        augmentor = AUGMENTATOR_MAPPING[n]
+        augmentor.set_augmentation_percentage(p / 100)
         if augmentor.operate_on_embeddings:
             augmentors_on_tokens.append(augmentor)
         else:

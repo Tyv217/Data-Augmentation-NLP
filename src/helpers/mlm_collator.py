1c9cb9b7dc3d0e8924d4008dbbadd3339f43c57a
Thomas <thomasyuan1@gmail.com>
2023-04-11 11:09:09 +0100

    Yes

--- src/helpers/mlm_collator.py
+++ src/helpers/mlm_collator.py
@@ -9,14 +9,14 @@ class MLMCollator(DataCollatorForWholeWordMask):
         attention_masks = torch.stack([input['attention_mask'] for input in inputs])
 
         batch = {
-            'input_id': input_ids.to(self.device),
-            'attention_mask': attention_masks.to(self.device),
+            'input_id': input_ids.to(device),
+            'attention_mask': attention_masks.to(device),
         }
 
         if self.mlm_probability is not None:
             input_ids = [input_id for input_id in batch['input_id']]
             masked_inputs = self.torch_call(input_ids)
-            batch['input_id'] = masked_inputs["input_ids"].long().to(self.device)
-            batch['label'] = masked_inputs["labels"].long().to(self.device)
+            batch['input_id'] = masked_inputs["input_ids"].long().to(device)
+            batch['label'] = masked_inputs["labels"].long().to(device)
 
         return batch

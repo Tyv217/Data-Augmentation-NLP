4d5edf04ca0b0a8bfe660b90fd5e35cf5c532040
Thomas <thomasyuan1@gmail.com>
2023-04-19 18:21:01 +0100

    Yes

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -308,6 +308,8 @@ def text_classify_with_saliency(args):
         print("FewShot Training Used. Samples per class:", args.samples_per_class)
     else:
         print("Dataset Percentage:", args.dataset_percentage)
+    
+    print("Invert saliency used:", False if args.invert_saliency == 0 else True)
 
     if args.output_saliency_results != 0:
 
@@ -352,7 +354,6 @@ def text_classify_with_saliency(args):
 
         highest_means = sorted(highest, key=lambda x: x[1], reverse=True)[:10]
         lowest_means = sorted(lowest, key=lambda x: x[1])[:10]
-        print("Invert saliency used:", False if args.invert_saliency == 0 else True)
         print('Top 10 highest saliency scores:', highest_means)
         print('Top 10 lowest saliency scores:', lowest_means)
 
